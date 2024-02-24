73407cd7aa1585c8abec2d9de11fe7a6e87a5827
Sudhanshu Singh <74963600+sudhanshusinghaiml@users.noreply.github.com>
2024-02-24 21:26:48 +0530

    After testing Multi User Prediction

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ from ML_Pipeline.OutlierTreatment import OutlierTreatment
 from ML_Pipeline.FeatureEncoder import FeatureEncoder
 
 from flask_cors import cross_origin
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 import pandas as pd
 import numpy as np
 
@@ -35,6 +35,8 @@ sys.modules['sklearn.utils.safe_indexing'] = sklearn.utils._safe_indexing
 # 1. Creating LoanEligibility Flask app
 BankingLoanEligibilityapp = Flask(__name__)
 
+UPLOAD_FOLDER = 'output'
+BankingLoanEligibilityapp.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # 2. Index route, opens automatically on http://127.0.0.1:8000
 @BankingLoanEligibilityapp.route('/')
@@ -57,61 +59,42 @@ def project_home():
     heading = 'Banking Loan Eligibility Prediction App'
     return render_template('/loanEligibilityProject.html', heading=heading)
 
-@BankingLoanEligibilityapp.route('/UploadMultiUserData') # , methods=['GET', 'POST'])
+@BankingLoanEligibilityapp.route('/multiUserLoanEligibilityPrediction.html')
 @cross_origin()
-def upload_multi_user_data_for_predicton():
+def upload_file():
+    return render_template('/multiUserLoanEligibilityPrediction.html')
+
+@BankingLoanEligibilityapp.route('/upload_processing', methods=['POST'])
+def upload_processing():
     status = ' '
     if request.method == "POST":
-        to_predict_dict = request.form.to_dict()
-        flag = predictor(to_predict_dict)
-        if flag:
-            status = "Loan Application can be accepted."
-        else:
-            status = "Loan Application is rejected."
+        file = request.files['csvfile']
+        if file:
+            filename = os.path.join(BankingLoanEligibilityapp.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
 
-    return render_template("/multiUserLoanEligibilityPrediction.html", model_training_status=status)
+            # Reading the uploaded CSV file in datframe
+            df = pd.read_csv(filename)
+            
+            # Calling the Loan Eligibility Prediction from the model
+            predicted_df = MultiUserLoanEligibilityPredictor.predictor(df)
+            
+            # Save the updated DataFrame to a new CSV file
+            processed_filename = os.path.join(BankingLoanEligibilityapp.config['UPLOAD_FOLDER'], 'processed_' + file.filename)
+            predicted_df.to_csv(processed_filename, index=False)
 
+            return redirect(url_for('display_multi_user_results', filename=processed_filename))
+        
+    return redirect(url_for('upload_file'))
 
 # 5. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return whether given customers is eligible for loan 
 #    (http://127.0.0.1:8000/multi-user-loan-eligibility-prediction)
-@BankingLoanEligibilityapp.route('/multiUserLoanEligibilityPredictionDisplay.html', methods=['GET', 'POST'])
-def multi_user_loan_eligibility_predictor():
-    if request.method == "POST":
-        try:
-            if 'csvfile' not in request.files:
-                return 'No file has been uploaded', 400
-            
-            file = request.files['csvfile']
-            if file.filename == '':
-                return 'Invalid file was provided', 400
-            
-            if file:
-                # Save the uploaded CSV file
-                file.save(file.filename)
-                
-                # Process the CSV file
-                df = pd.read_csv(file.filename)
-                
-                # Get the Loan Eligibility Prediction from the model
-                predicted_data = MultiUserLoanEligibilityPredictor.predictor(df)
-                
-                # Save the modified data to a new CSV file
-                output_filename = 'MultiUserLoanEligibilityPrediction.csv'
-                predicted_data.to_csv(output_filename, index=False)
-                
-            else:
-                return 'File upload failed', 400
-                
-        except Exception as e: # Exception for system errors
-            print('Exception in multi_user_loan_eligibility_predictor', e)
-        
-    # Render the HTML template with the modified data
-    # return render_template("/multiUserLoanEligibilityPrediction.html", data=predicted_data.to_html())
-    if predicted_data is not None:
-        return render_template("/multiUserLoanEligibilityPredictionDisplay.html", data=predicted_data.to_html())
-    else:
-        return render_template("/multiUserLoanEligibilityPrediction.html")
+@BankingLoanEligibilityapp.route('/display_multi_user_results/<filename>')
+def display_multi_user_results(filename):
+    df = pd.read_csv(filename)
+    data = df.to_dict(orient='records')
+    return render_template('/multiUserLoanEligibilityPredictionDisplay.html', data=data)
 
     
 # 6. Expose the prediction functionality, make a prediction from the passed
